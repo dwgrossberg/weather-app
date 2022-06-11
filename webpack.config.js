a49e4979bbf5825e12e9ac6efb7168e2c1c7f73b
Dan Grossberg <dwgrossberg@gmail.com>
2022-06-11 12:18:51 +0700

    Initial directory commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-dupe-keys */
+/* eslint-disable no-undef */
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    // clean: true,
+  },
+  plugins: [],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
