01c21026c187c900909503673cd9d70caa577d22
Dan Grossberg <dwgrossberg@gmail.com>
2022-06-11 23:06:28 +0700

    Add setBackground function to correspond weather conditions with backgroundImg gifs

--- src/ui.js
+++ src/ui.js
@@ -1,9 +1,16 @@
 import fetchWeatherAPI from "./fetchAPI.js";
+import clouds from "./assets/clouds.gif";
+import drizzle from "./assets/drizzle.gif";
+import thunderstorm from "./assets/thunderstorm.gif";
+import rain from "./assets/rain.gif";
+import snow from "./assets/snow.gif";
+import mist from "./assets/fog.gif";
+import clear from "./assets/clear-sky.gif";
 
 const displayController = (() => {
   const setWeather = async () => {
     // Default weather on page load
-    const weatherData = await fetchWeatherAPI.getWeather("bangkok");
+    const weatherData = await fetchWeatherAPI.getWeather("beijing");
     console.log(weatherData);
     setBackground(weatherData.weather[0].main);
   };
@@ -12,17 +19,16 @@ const displayController = (() => {
   const setBackground = (weather) => {
     switch (weather) {
       case "Thunderstorm":
-        document.body.style.backgroundImage =
-          "url('./assets/thunderstorm.gif')";
+        document.body.style.backgroundImage = `url(${thunderstorm})`;
         break;
       case "Drizzle":
-        document.body.style.backgroundImage = "./assets/drizzle.gif";
+        document.body.style.backgroundImage = `url(${drizzle})`;
         break;
       case "Rain":
-        document.body.style.backgroundImage = "./assets/rain.gif";
+        document.body.style.backgroundImage = `url(${rain})`;
         break;
       case "Snow":
-        document.body.style.backgroundImage = "./assets/snow.gif";
+        document.body.style.backgroundImage = `url(${snow})`;
         break;
       case "Mist":
       case "Smoke":
@@ -33,14 +39,13 @@ const displayController = (() => {
       case "Ash":
       case "Squall":
       case "Tornado":
-        document.body.style.backgroundImage = "./assets/drizzle.gif";
+        document.body.style.backgroundImage = `url(${mist})`;
         break;
       case "Clear":
-        document.body.style.backgroundImage = "./assets/clear-sky.gif";
+        document.body.style.backgroundImage = `url(${clear})`;
         break;
       case "Clouds":
-        // document.body.style.backgroundImage = "url(./assets/clouds.gif)";
-        console.log(document.body.style.backgroundImage);
+        document.body.style.backgroundImage = `url(${clouds})`;
         break;
     }
   };
